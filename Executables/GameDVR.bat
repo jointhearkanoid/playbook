dbf16503cb047b3d43de3767812cf39763a7abe1
Rayden_Berzerk409 <juanbogallo1113826@correo.itm.edu.co>
2024-03-23 17:55:19 -0500

    Add files via upload

---
+++ Executables/GameDVR.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+
+:: Navegar a la clave del Registro y obtener el valor de EditionID
+for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v EditionID 2^>nul ^| find "EditionID"') do (
+    set "EditionID=%%a"
+)
+
+:: Verificar el valor de EditionID y realizar las acciones correspondientes
+if /i "%EditionID%"=="Professional" (
+    reg import "Desactivar la barra de juegos (opcional).reg"
+    goto :end
+) elseif /i "%EditionID%"=="Home" (
+    REGEDIT /s "Desactivar la barra de juegos (opcional).reg"
+    goto :end
+) else (
+    echo La edición no coincide con ninguna acción definida.
+)
+
+:end
+endlocal
